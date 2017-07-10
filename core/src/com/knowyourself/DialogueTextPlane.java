3b86dd158611e3ee1b97046da47be35681fd3705
rocketfish <rocket.fish@mail.com>
2017-07-09 20:08:39 -0400

    Dialogue Text  Area is now Available

---
+++ core/src/com/knowyourself/DialogueTextPlane.java
@@ -0,0 +1,53 @@
+package com.knowyourself;
+
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.kotcrab.vis.ui.util.TableUtils;
+import com.kotcrab.vis.ui.widget.ScrollableTextArea;
+import com.kotcrab.vis.ui.widget.VisScrollPane;
+import com.kotcrab.vis.ui.widget.VisTable;
+import com.kotcrab.vis.ui.widget.VisWindow;
+
+public class DialogueTextPlane extends VisWindow{
+    Viewport vpReference;
+    public DialogueTextPlane(String title, Viewport viewport) {
+        super(title, false);
+        vpReference = viewport;
+        TableUtils.setSpacingDefaults(this);
+        columnDefaults(0).left();
+        addVisWidgets();
+
+        setResizable(true);
+        setSize(viewport.getScreenWidth(), viewport.getScreenHeight()/5);
+        setPosition(0, 0);
+//        setSize(sizeX, sizeY);
+//        setPosition(228, 300);
+        addListener(new GameScreen.WindowResizeListener() {
+            @Override
+            public void resize () {
+                updateSize();
+            }
+        });
+    }
+
+    public void updateSize() {
+        setSize(vpReference.getScreenWidth(), vpReference.getScreenHeight()/5);
+    }
+
+    private void addVisWidgets () {
+        ScrollableTextArea textArea = new ScrollableTextArea("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis odio.\nFun thing: This text area supports scrolling.");
+
+        // ---
+
+        VisTable table = new VisTable();
+
+        VisScrollPane scrollPane = new VisScrollPane(table);
+        scrollPane.setFlickScroll(false);
+        scrollPane.setFadeScrollBars(false);
+
+        // ---
+
+        add(textArea.createCompatibleScrollPane()).growX().growY().row();
+        add(scrollPane).spaceTop(8).growX().growY().row();
+    }
+
+}
--- core/src/com/knowyourself/GameScreen.java
+++ core/src/com/knowyourself/GameScreen.java
@@ -1,13 +1,17 @@
 package com.knowyourself;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.FileHandleResolver;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.bearfishapps.libs.GeneralScreens;
 import com.knowyourself.utils.AssetFinder;
+import com.kotcrab.vis.ui.VisUI;
+import com.kotcrab.vis.ui.widget.PopupMenu;
 
 public class GameScreen extends GeneralScreens {
 
@@ -31,12 +35,65 @@ public class GameScreen extends GeneralScreens {
     }
 
     @Override
-    public void preShow(Table table, InputMultiplexer multiplexer) {
+    public void preShow(final Table table, InputMultiplexer multiplexer) {
+        VisUI.load(VisUI.SkinScale.X1);
+        table.add().expand().fill();
+
+//        stage.addActor(new TestTextAreaAndScroll());
+        stage.addActor(new DialogueTextPlane("Someone Said", viewport));
+
+        stage.addListener(new InputListener() {
+            boolean debug = false;
+
+            @Override
+            public boolean keyDown (InputEvent event, int keycode) {
+                if (keycode == Input.Keys.F12) {
+                    debug = !debug;
+                    table.setDebug(debug, true);
+                    for (Actor actor : stage.getActors()) {
+                        if (actor instanceof Group) {
+                            Group group = (Group) actor;
+                            group.setDebug(debug, true);
+                        }
+                    }
+                    return true;
+                }
+
+                return false;
+            }
+        });
 
     }
 
     @Override
     public void destroy() {
-
+        VisUI.dispose();
     }
+
+    @Override
+    public void resize (int width, int height) {
+        if (width == 0 && height == 0) return;
+        stage.getViewport().update(width, height, true);
+
+        PopupMenu.removeEveryMenu(stage);
+        WindowResizeEvent resizeEvent = new WindowResizeEvent();
+        for (Actor actor : stage.getActors()) {
+            actor.fire(resizeEvent);
+        }
+    }
+
+    abstract static class WindowResizeListener implements EventListener {
+        @Override
+        public boolean handle (Event event) {
+            if (event instanceof WindowResizeEvent == false) return false;
+            resize();
+            return false;
+        }
+
+        public abstract void resize ();
+    }
+
+    class WindowResizeEvent extends Event {
+    }
+
 }
